443b6421e8edc2c5caf228eec7e7331ecca8847d
Jamie McCrindle <jamiemccridle@gmail.com>
2012-04-26 13:14:57 +0100

    fixed loader and removed twitter

--- assets/themes/lawa/js/slider3.js
+++ assets/themes/lawa/js/slider3.js
@@ -2,7 +2,8 @@
 $(function(){
 	$('.ad-gallery').adGallery({
 		display_next_and_prev: false,
-		slideshow: {
+        loader_image: '/assets/themes/lawa/images/loader.gif',
+        slideshow: {
 		    enable: true,
 		    autostart: true,
 		    speed: 5000}
---
+++ shoes/aw12/anoush_blue.md
@@ -0,0 +1,8 @@
+---
+layout: shoe
+category : shoes
+tags : [anoush, blue, peep]
+---
+{% include JB/setup %}
+
+Test
